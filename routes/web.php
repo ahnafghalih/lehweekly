e6f1fd8f13adef7d8ab1b985dc3a4b1e6d0386cc
lehweekly <anaffathi23@gmail.com>
2026-09-22 19:35:54 +0700

    update

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/// leh.com/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
